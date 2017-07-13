5b01093b50672e0e126834f08bbc3b3857b436e3
yKicchan <yKicchanApp@gmail.com>
2017-07-13 15:14:22 +0900

    add:[test.php]トップページとDBテストを別ページに変更

---
+++ docker/src/test.php
@@ -0,0 +1,18 @@
+<?php
+
+$master  = new mysqli('master', 'user', 'password', 'database');
+$replica = new mysqli('replica', 'user', 'password', 'database');
+
+$sql = "INSERT INTO `test` (`time`) VALUES ('".date('Y/m/d H:i:s')."')";
+$master->query($sql);
+
+$sql = "SELECT * FROM `test`";
+if ($result = $replica->query($sql)) {
+    while ($row = $result->fetch_assoc()) {
+        echo "{$row['id']}: {$row['time']}<br>";
+    }
+    $result->close();
+}
+
+$master->close();
+$replica->close();
