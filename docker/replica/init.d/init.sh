2c7ddc540252ec64d94dc893d72fdfdd6251ef52
yKicchan <yKicchanApp@gmail.com>
2017-07-13 12:28:12 +0900

    modified:[DB]起動時のバグを修正

--- docker/replica/init.d/init.sh
+++ docker/replica/init.d/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 rm -f /var/lib/mysql/auto.cnf
+while [ ! -f "/docker/master_db.sql" ]
+do
+    sleep 3;
+done
 mysql -u root -prootpass database < /docker/master_db.sql
 MASTER_LOG_FILE=`head -n 100 /docker/master_db.sql | grep -o "MASTER_LOG_FILE='mysql-bin.[0-9]\+'"`
 MASTER_LOG_POS=`head -n 100 /docker/master_db.sql | grep -o "MASTER_LOG_POS=[0-9]\+"`
--- docker/src/index.php
+++ docker/src/index.php
@@ -1,2 +1,17 @@
 <?php
-phpinfo();
+$master  = new mysqli('master', 'user', 'password', 'database');
+$replica = new mysqli('replica', 'user', 'password', 'database');
+
+$sql = "INSERT INTO user (name) VALUES ('".gethostname()."')";
+$master->query($sql);
+
+$sql = "SELECT id, host FROM test";
+if ($result = $replica->query($sql)) {
+    while ($row = $result->fetch_assoc()) {
+        echo "{$row['id']}: {$row['host']}<br>";
+    }
+    $result->close();
+}
+
+$master->close();
+$replica->close();
